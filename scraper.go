2876bcc03f2ac23971e612089a5dea7a8bd996a4
Poompichayout Kongpiam <poompichayout@gmail.com>
2023-05-19 15:27:47 +0700

    feat: fetched posts from internet and save to database

--- scraper.go
+++ scraper.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"github.com/google/uuid"
 	"github.com/poompichayout/rss-aggregator/internal/database"
 	"log"
+	"strings"
 	"sync"
 	"time"
 )
@@ -51,7 +54,39 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post", item.Title, "on feed", feed.Name)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		if item.PubDate == "" {
+			log.Printf("published date of feed %v is blank string", feed.Name)
+			continue
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("couldn't parse date %v with err: %v", item.PubDate, err)
+			continue
+		}
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description,
+			PublishedAt: pubAt,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
+			log.Println("failed to create post:", err)
+		}
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 }
