484e5873f460af0cf2f45c13e1061012b9509f80
Poompichayout Kongpiam <poompichayout@gmail.com>
2023-05-19 14:05:28 +0700

    feat: delete feed_follows routes

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 	"github.com/poompichayout/rss-aggregator/internal/database"
 	"net/http"
@@ -44,4 +45,23 @@ func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Re
 		return
 	}
 	respondWithJSON(w, 200, databaseFeedFollowsToFeedFollows(feeds))
+}
+
+func (apiCfg *apiConfig) handlerDeleteFeedFollows(w http.ResponseWriter, r *http.Request, user database.User) {
+	feedFollowIDStr := chi.URLParam(r, "feedFollowID")
+	feedFollowID, err := uuid.Parse(feedFollowIDStr)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprint("Couldn't parse feed follow id:", err))
+		return
+	}
+
+	err = apiCfg.DB.DeleteFeedFollow(r.Context(), database.DeleteFeedFollowParams{
+		ID:     feedFollowID,
+		UserID: user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprint("Couldn't delete feed follows:", err))
+		return
+	}
+	respondWithJSON(w, 200, struct{}{})
 }
--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -4,4 +4,7 @@ VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
 
 -- name: GetFeedFollows :many
-SELECT * from feed_follows WHERE user_id=$1;
+SELECT * from feed_follows WHERE user_id = $1;
+
+-- name: DeleteFeedFollow :exec
+DELETE FROM feed_follows WHERE id = $1 AND user_id = $2;
